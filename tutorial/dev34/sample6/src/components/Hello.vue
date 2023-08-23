d7721070d3f9c81bc9214a2877589128ce4696fd
NaxHugltx3 <naoki.k.0903@gmail.com>
2023-08-23 20:54:23 +0900

    [6.6] データ受け渡し処理

--- tutorial/dev34/sample6/src/components/Hello.vue
+++ tutorial/dev34/sample6/src/components/Hello.vue
@@ -1,9 +1,16 @@
 <script setup>
 import { inject } from "vue"
+import { useRouter } from "vue-router"
 
 const message = inject("message")
+
+const router = useRouter()
+const onClick = () => {
+  router.push({ name: "home" })
+}
 </script>
 
 <template>
   <div>{{ message }}</div>
+  <button type="button" @click="onClick" >戻る</button>
 </template>
